818a43414f9403a4d24ed52a4810340e92cd9eaa
Alex Wang <alxw1863@berkeley.edu>
2019-02-11 10:59:36 -0800

    part 1 complete

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -7,9 +7,19 @@ class HangpersonGame
 
   # def initialize()
   # end
+
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
+  attr_accessor :word_with_guesses
+  attr_accessor :check_win_or_lose
   
   def initialize(word)
-    @word = word
+    @word = word.downcase
+    @guesses = ''
+    @wrong_guesses = ''
+    @word_with_guesses = '-'*@word.length
+    @check_win_or_lose = :play
   end
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
@@ -22,6 +32,45 @@ class HangpersonGame
     Net::HTTP.new('watchout4snakes.com').start { |http|
       return http.post(uri, "").body
     }
+  end
+
+  def guess(letters)
+    if letters == '' or letters == nil
+      raise ArgumentError, 'Input is blank'
+    end
+    letters = letters.split('')
+    counter = 0
+    while counter < letters.length
+      letter = letters[counter].downcase
+      if @guesses.include? letter or @wrong_guesses.include? letter
+        return false
+      elsif !(letter =~ /[A-Za-z]/)
+        raise ArgumentError, 'Input is not a letter'
+      end
+      if @word.include? letter 
+        @guesses += letter
+      else
+        @wrong_guesses += letter
+      end
+      counter += 1
+    end
+    displayed = ''
+    counter = 0
+    while counter < @word.length
+      if @guesses.include? word[counter]
+        displayed += word[counter]
+      else
+        displayed += '-'
+      end
+      counter += 1
+    end
+    @word_with_guesses = displayed
+    if @wrong_guesses.length >= 7
+      @check_win_or_lose = :lose
+    elsif not @word_with_guesses.include? '-'
+      @check_win_or_lose = :win
+    end
+    return true
   end
 
 end
